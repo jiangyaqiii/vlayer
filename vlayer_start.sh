f27bfb42a43c44adf73810b411bcae5db1388f09
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2025-04-16 17:41:18 +0800

    Update vlayer_start.sh

--- vlayer_start.sh
+++ vlayer_start.sh
@@ -33,4 +33,5 @@ bun add @vlayer/sdk
 
 # Install Vlayer SDK & Run Prover
 # bun run prove:testnet
-screen -d -m -S vlayer5 bash -c "cd /$project_name/vlayer && while true; do bun run prove:testnet; sleep 3600; done"
+read -p "screen名称: " screen_name
+screen -d -m -S $screen_name bash -c "cd /$project_name/vlayer && while true; do bun run prove:testnet; sleep 3600; done"
